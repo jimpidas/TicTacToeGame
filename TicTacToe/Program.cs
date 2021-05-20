2cdda962dac136055f6b4eb4e393a9ac62944c7d
unknown <jimpidas>
2021-05-20 12:09:58 +0530

    [Jyosmita]Added:User will be able to see the board so that he can give input in specified place

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -11,6 +11,16 @@ namespace Tic_Tac_Toe
             {
                 board[i] = ' ';
             }
+            Console.WriteLine("    |      |    ");
+            Console.WriteLine(" {0}  |  {1}   |  {2} ", board[1], board[2], board[3]);
+            Console.WriteLine("____|______|____");
+            Console.WriteLine("    |      |    ");
+            Console.WriteLine(" {0}  |  {1}   |   {2}  ", board[4], board[5], board[6]);
+            Console.WriteLine("____|______|____");
+            Console.WriteLine("    |      |    ");
+            Console.WriteLine(" {0}  |  {1}   |   {2}  ", board[7], board[8], board[9]);
+            Console.WriteLine("____|______|____");
+
         }
         static void ChooseLetter()
         {
